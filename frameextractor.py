4c25a2d616aa76737e2f0616a5bff3529569edb9
mkgopala <midhun.gopalakrishnan@outlook.com>
2021-05-02 18:43:04 -0700

    changes6

--- frameextractor.py
+++ frameextractor.py
@@ -37,12 +37,22 @@ def frameExtractor(videopath):
     frameList  = []
     cap = cv2.VideoCapture(videopath)
     video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    for pct in range(1, 10):
-        # print("Extracting frame..\n")
-        frame_no = round(video_length*pct/10);
+    if video_length > 10:
+        for pct in range(1, 10):
+            # print("Extracting frame..\n")
+            frame_no = round((video_length*pct)*0.1);
+            if frame_no == 0:
+                frame_no = 1
+            cap = cv2.VideoCapture(videopath)
+            cap.set(1, frame_no)
+            ret, frame = cap.read()
+            frameList.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+    else:
+        frame_no = int(video_length * 0.5)
         cap.set(1, frame_no)
         ret, frame = cap.read()
         frameList.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+    cap.release()
     return frameList
 
 
@@ -53,8 +63,9 @@ def frameExtractorSOT(videopath):
     # cutLocation = trainFrame.get(video_file_name[1], 0.5)
     # frame_no = int(video_length * cutLocation)
     # print("Extracting frame..\n")
-    frame_no = int(video_length * 0.4)
+    frame_no = int(video_length * 0.5)
     cap.set(1, frame_no)
     ret, frame = cap.read()
+    cap.release()
     return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
